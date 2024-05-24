4258ef13d69157ffe07c54a34eab49e61e52388e
Dennis Koay <thecvcoder@foxmail.com>
2024-05-24 23:35:33 +0800

    创建Navbar组件&logos使用Image显示

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,37 +2,24 @@ import clsx from 'clsx'
 import { SITE_TITLE } from '../config'
 import { NavLink } from './NavLink'
 import { IconMenu } from './icons'
+import Image from 'next/image'
+import { Navbar } from './Navbar'
 
 interface Props {
   className?: string
 }
 export const Header: React.FC<Props> = ({ className }) => {
   return (
-    <header className={clsx('sticky top-0 border-b bg-white/80 max-md:bg-white/90 backdrop-blur-2xl', className)}>
+    <header className={clsx('sticky top-0 border-b bg-white/80 max-md:bg-white/90 backdrop-blur-2xl box-border', className)}>
       <div className='md:max-w-[var(--header-desktop-max-width)] py-3 mx-auto flex max-md:justify-center justify-between items-center max-md:px-4'>
         <h1>
-          <a href="/" className='block w-[80px] h-[80px] object-cover bg-cover bg-[url("/LOGO.svg")] indent-[-9999em]'>
-            {SITE_TITLE}
+          <a href="/">
+            <Image src="/LOGO.svg" alt={SITE_TITLE} width={80} height={80} priority decoding='async' />
           </a>
         </h1>
-        {/* <a href="/" className='md:hidden px-2.5 py-1 border-2 border-[#8a8a8a] rounded-md'>
-          <IconMenu color='#cccccc' width={40} height={40} />
-        </a> */}
-        <nav className='grid grid-cols-5 gap-2 max-md:hidden'>
-          <NavLink href='/'>Home</NavLink>
-          <NavLink href='/b'>Music</NavLink>
-          <NavLink href='/a'>About</NavLink>
-          <NavLink href='/c'>News</NavLink>
-          <NavLink href='/d'>Uses</NavLink>
-        </nav>
+        <Navbar className='max-md:hidden gap-2' />
       </div>
-      <nav className='md:hidden h-[48px] items-stretch grid grid-cols-5 gap-0'>
-          <NavLink href='/'>Home</NavLink>
-          <NavLink href='/b'>Music</NavLink>
-          <NavLink href='/a'>About</NavLink>
-          <NavLink href='/c'>News</NavLink>
-          <NavLink href='/d'>Uses</NavLink>
-        </nav>
+      <Navbar className='md:hidden h-[48px]' />
     </header>
   )
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import clsx from "clsx"
+import { NavLink } from "./NavLink"
+
+interface Props {
+    className?: string
+}
+export const Navbar: React.FC<Props> = ({ className }) => {
+    return (
+        <nav className={clsx('grid grid-cols-5', className)}>
+            <NavLink href='/'>Home</NavLink>
+            <NavLink href='/music'>Music</NavLink>
+            <NavLink href='/about'>About</NavLink>
+            <NavLink href='/news'>News</NavLink>
+            <NavLink href='/uses'>Uses</NavLink>
+        </nav>
+    )
+}
