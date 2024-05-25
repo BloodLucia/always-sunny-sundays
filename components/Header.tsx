0232d69c7c9862b4f4742bd77aca4990f4ccf495
Dennis <thecvcoder@foxmail.com>
2024-05-25 20:04:10 +0800

    修改Header透明度

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,8 +11,8 @@ interface Props {
 export const Header: React.FC<Props> = ({ className }) => {
   const [menuOpen, setMenuOpen] = useState(false)
   return (
-    <header className={clsx('sticky top-0 border-b bg-white box-border', className)}>
-      <div className="md:max-w-[var(--header-desktop-max-width)] py-3 mx-auto flex justify-between items-center max-md:px-4 relative">
+    <header className={clsx('sticky top-0 border-b box-border', className)}>
+      <div className="md:max-w-[var(--header-desktop-max-width)] max-md:backdrop-blur-md backdrop-blur-2xl bg-white/[0.85] py-3 mx-auto flex justify-between items-center max-md:px-4 relative">
         <Brand />
         <Navbar className="max-md:hidden gap-2" />
         <button onClick={() => setMenuOpen(!menuOpen)} className='md:hidden'>
